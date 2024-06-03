44dc2e23b6fad3814ea1e3cc5bb687c7a1269a7c
Amir <a.rahimi@live.ca>
2024-06-03 11:46:36 -0700

    update 1.5  Added automation to automatically do simulation based each file in Price_History folder.

--- Scenario1DCA.py
+++ Scenario1DCA.py
@@ -1,11 +1,9 @@
 import csv
-import pandas as pd
 from datetime import datetime, timedelta
+import pandas as pd
 import os
 
-# in this scenario, we'll be only buying based on the weekly percentage difference of bitcoin based on the cash we hold
-# e.g. bitcoin weekly change is 3% and have $100,000, we buy 3,000$ worth of bitcoin.
-
+# in this Scenario, we take our million dollars and divide it into 205 equal amounts and purchase that weekly
 
 price_dict = {}
 bitcoin_wallet = [0]
@@ -13,15 +11,18 @@ cash_wallet = [1000000]
 transaction_date = [0]
 transaction_bitcoin_price = [0]
 transaction_fee = [0]
+dca_amount = 4878
+dca_fee = dca_amount * .02
 
 
-def scenario3(filename):
+def scenario1DCA(filename):
     with open(filename, mode='r') as file:
         csv_reader = csv.reader(file)
         next(csv_reader)
         for row in csv_reader:
             date = row[0]
             price = float(row[1])
+
             price_dict[date] = price
 
     def current_price(current_date):
@@ -32,35 +33,24 @@ def scenario3(filename):
         last_week_date = start_date + timedelta(days=-7)
         return price_dict[last_week_date.strftime("%Y-%m-%d")]
 
-    def percent_change(current_price, last_week_price):
-        return ((current_price - last_week_price) / last_week_price)
+    def cash_wallet_change():
+        global cash_wallet, dca_amount
+        current_cash = cash_wallet[len(cash_wallet) - 1]
+        purchase_fee = dca_amount * 0.02
+        new_cash_amount = current_cash - dca_amount - purchase_fee
+        cash_wallet.append(new_cash_amount)
 
-    def cash_wallet_change(percent_change):
-        global cash_wallet
-        if percent_change < 0:
-            current_cash = cash_wallet[len(cash_wallet) - 1]
-            amount_to_buy = current_cash * (percent_change * -1)
-            purchase_fee = amount_to_buy * .02
-            new_cash_amount = current_cash - (amount_to_buy + purchase_fee)
-            transaction_fee.append(purchase_fee)
-            cash_wallet.append(new_cash_amount)
-            return float(amount_to_buy)
-        else:
-            cash_wallet.append(cash_wallet[len(cash_wallet) - 1])
-            transaction_fee.append(0)
-            amount_to_buy = 0
-            return float(amount_to_buy)
-
-    def bitcoin_wallet_change(amount_to_buy, current_price):
-        global bitcoin_wallet
+    def bitcoin_wallet_change(current_price):
+        global bitcoin_wallet, dca_amount
         current_bitcoin_owned = bitcoin_wallet[len(bitcoin_wallet) - 1]
-        new_bitcoin_added = amount_to_buy / current_price
-        new_bitcoin_owned = current_bitcoin_owned + new_bitcoin_added
-        bitcoin_wallet.append(new_bitcoin_owned)
+        bitcoin_to_purchase = dca_amount / current_price
+        new_bitcoin_total = current_bitcoin_owned + bitcoin_to_purchase
+        bitcoin_wallet.append(new_bitcoin_total)
 
     def update_lists(date):
         transaction_date.append(date)
         transaction_bitcoin_price.append(current_price(date))
+        transaction_fee.append(dca_fee)
 
     def results():
         df = pd.DataFrame({
@@ -72,7 +62,7 @@ def scenario3(filename):
         })
         change_directory = filename.replace('Price_History', "Results")
         excel_file = change_directory.replace(".csv", "Results.xlsx")
-        sheet_name = 'Scenario3'
+        sheet_name = 'Scenario1DCA'
         if not os.path.isfile(excel_file):
             with pd.ExcelWriter(excel_file, engine='openpyxl') as writer:
                 df.to_excel(writer, sheet_name=sheet_name, index=False)
@@ -90,8 +80,8 @@ def scenario3(filename):
 
             if day % 7 == 0:
                 update_lists(date)
-                bitcoin_wallet_change(cash_wallet_change(percent_change(current_price(date), last_week_price(date))),
-                                      current_price(date))
+                cash_wallet_change()
+                bitcoin_wallet_change(current_price(date))
                 day += 1
             else:
                 day += 1
--- Scenario2.py
+++ Scenario2.py
@@ -1,6 +1,7 @@
 import csv
 import pandas as pd
 from datetime import datetime, timedelta
+import os
 
 # in this scenario, we'll be buying and selling based on the weekly percentage difference of bitcoin based on the cash we hold
 # e.g. bitcoin weekly change is 3% and have $100,000, we sell 3,000$ worth of bitcoin. we buy that amount if its -3%
@@ -13,85 +14,85 @@ transaction_date = [0]
 transaction_bitcoin_price = [0]
 transaction_fee = [0]
 
-with open('BITCOIN(05-2020-04-2024).csv', mode='r') as file:
-    csv_reader = csv.reader(file)
-    next(csv_reader)
-    for row in csv_reader:
-        date = row[0]
-        price = float(row[1])
-        price_dict[date] = price
 
+def scenario2(filename):
+    with open(filename, mode='r') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        for row in csv_reader:
+            date = row[0]
+            price = float(row[1])
+            price_dict[date] = price
 
-def current_price(current_date):
-    return float(price_dict[current_date])
+    def current_price(current_date):
+        return float(price_dict[current_date])
 
+    def last_week_price(current_date):
+        start_date = datetime.strptime(current_date, "%Y-%m-%d")
+        last_week_date = start_date + timedelta(days=-7)
+        return price_dict[last_week_date.strftime("%Y-%m-%d")]
 
-def last_week_price(current_date):
-    start_date = datetime.strptime(current_date, "%Y-%m-%d")
-    last_week_date = start_date + timedelta(days=-7)
-    return price_dict[last_week_date.strftime("%Y-%m-%d")]
+    def percent_change(current_price, last_week_price):
+        return (current_price - last_week_price) / last_week_price
 
-
-def percent_change(current_price, last_week_price):
-    return (current_price - last_week_price) / last_week_price
-
-
-def cash_wallet_change(percent_change):
-    global cash_wallet, transaction_fee
-    # if price goes up we sell bitcoin therefore gain money
-    current_cash = cash_wallet[len(cash_wallet) - 1]
-    amount_to_sell_or_buy = current_cash * percent_change
-    selling_fee = amount_to_sell_or_buy * .02
-    new_cash_amount = current_cash + amount_to_sell_or_buy - selling_fee
-    cash_wallet.append(new_cash_amount)
-    transaction_fee.append(selling_fee)
-    if float(amount_to_sell_or_buy) < 0:
-        return float(amount_to_sell_or_buy * -1)
-    else:
-        return float(amount_to_sell_or_buy)
-
-def update_lists(date):
-    transaction_date.append(date)
-    transaction_bitcoin_price.append(current_price(date))
-
-
-def bitcoin_wallet_change(cash_wallet_change, current_price):
-    global bitcoin_wallet
-    current_bitcoin_owned = bitcoin_wallet[len(bitcoin_wallet) - 1]
-    new_bitcoin_added = cash_wallet_change / current_price
-    new_bitcoin_owned = current_bitcoin_owned + new_bitcoin_added
-    bitcoin_wallet.append(new_bitcoin_owned)
-
-
-def results():
-    df = pd.DataFrame({
-        'transaction_date': transaction_date,
-        'bitcoin_wallet': bitcoin_wallet,
-        'cash_wallet': cash_wallet,
-        'bitcoin_price': transaction_bitcoin_price,
-        'transaction_fee': transaction_fee
-    })
-    excel_file = 'results(2020-2024).xlsx'
-    with pd.ExcelWriter(excel_file, engine='openpyxl', mode='a') as writer:
-        df.to_excel(writer, sheet_name='Scenario3', index=False)
-
-
-def main_iteration():
-    global purchases, transaction_date, transaction_bitcoin_price
-
-    day = 1
-    for date in sorted(price_dict):
-        date = datetime.strptime(date, "%Y-%m-%d") + timedelta(days=+1)
-        date = date.strftime("%Y-%m-%d")
-
-        if day % 7 == 0:
-            update_lists(date)
-            bitcoin_wallet_change(cash_wallet_change(percent_change(current_price(date), last_week_price(date))),
-                                  current_price(date))
-            day += 1
+    def cash_wallet_change(percent_change):
+        global cash_wallet, transaction_fee
+        # if price goes up we sell bitcoin therefore gain money
+        current_cash = cash_wallet[len(cash_wallet) - 1]
+        amount_to_sell_or_buy = current_cash * percent_change
+        if amount_to_sell_or_buy < 0:
+            selling_fee = amount_to_sell_or_buy * .02 * -1
         else:
-            day += 1
-    results()
+            selling_fee = amount_to_sell_or_buy * .02
+        new_cash_amount = current_cash + amount_to_sell_or_buy - selling_fee
+        cash_wallet.append(new_cash_amount)
+        transaction_fee.append(selling_fee)
+        return amount_to_sell_or_buy
 
+    def update_lists(date):
+        transaction_date.append(date)
+        transaction_bitcoin_price.append(current_price(date))
 
-main_iteration()
+    def bitcoin_wallet_change(cash_wallet_change, current_price):
+        global bitcoin_wallet
+        current_bitcoin_owned = bitcoin_wallet[len(bitcoin_wallet) - 1]
+        new_bitcoin_added = cash_wallet_change / current_price
+        new_bitcoin_owned = current_bitcoin_owned + new_bitcoin_added
+        bitcoin_wallet.append(new_bitcoin_owned)
+
+    def results():
+        df = pd.DataFrame({
+            'transaction_date': transaction_date,
+            'bitcoin_wallet': bitcoin_wallet,
+            'cash_wallet': cash_wallet,
+            'bitcoin_price': transaction_bitcoin_price,
+            'transaction_fee': transaction_fee
+        })
+        change_directory = filename.replace('Price_History', "Results")
+        excel_file = change_directory.replace(".csv", "Results.xlsx")
+        sheet_name = 'Scenario2'
+        if not os.path.isfile(excel_file):
+            with pd.ExcelWriter(excel_file, engine='openpyxl') as writer:
+                df.to_excel(writer, sheet_name=sheet_name, index=False)
+        else:
+            with pd.ExcelWriter(excel_file, engine='openpyxl', mode='a') as writer:
+                df.to_excel(writer, sheet_name=sheet_name, index=False)
+
+    def main_iteration():
+        global purchases, transaction_date, transaction_bitcoin_price
+
+        day = 1
+        for date in sorted(price_dict):
+            date = datetime.strptime(date, "%Y-%m-%d") + timedelta(days=+1)
+            date = date.strftime("%Y-%m-%d")
+
+            if day % 7 == 0:
+                update_lists(date)
+                bitcoin_wallet_change(cash_wallet_change(percent_change(current_price(date), last_week_price(date))),
+                                      current_price(date))
+                day += 1
+            else:
+                day += 1
+        results()
+
+    main_iteration()
