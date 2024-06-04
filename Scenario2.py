692df6947341a5c6221fa0bd305869fca8dba913
Amir <a.rahimi@live.ca>
2024-06-04 16:37:56 -0700

    update 1.7 fixed Scenario2.py to ensure assets stay above 0

--- Scenario2.py
+++ Scenario2.py
@@ -38,50 +38,84 @@ def scenario2(filename):
     def percent_change(current_price, last_week_price):
         return (current_price - last_week_price) / last_week_price
 
-    def selling_fees(amount_to_sell_or_buy):
-        if amount_to_sell_or_buy < 0:
-            selling_fee = amount_to_sell_or_buy * .02 * -1
+    def is_asset_enough(percent_change, current_price):
+        if percent_change < 0:
+            # if price goes down we buy and because we are always buying a percentage of our current money we will
+            # not run out of money
+            return True
         else:
-            selling_fee = amount_to_sell_or_buy * .02
-        return float(selling_fee)
+            # if price goes up we sell, so we need to make sure out bitcoin is enough
+            money_owned_currently = cash_wallet[len(cash_wallet) - 1]
+            bitcoin_to_sell = percent_change * money_owned_currently / current_price
+            bitcoin_owned_currently = bitcoin_wallet[len(bitcoin_wallet) - 1]
+            if (bitcoin_owned_currently - bitcoin_to_sell) < 0:
+                return False
+            else:
+                return True
 
-    def cash_wallet_change(percent_change):
-        global cash_wallet, transaction_fee
-        # if price goes up we sell bitcoin therefore gain money by taking profit
-        current_cash = cash_wallet[len(cash_wallet) - 1]
-        amount_to_sell_or_buy = current_cash * percent_change
-        selling_fee = selling_fees(amount_to_sell_or_buy)
-        new_cash_amount = current_cash + amount_to_sell_or_buy - selling_fee
-        percent_changes.append(percent_change)
-        cash_change.append(amount_to_sell_or_buy)
-        cash_wallet.append(new_cash_amount)
-        transaction_fee.append(selling_fee)
-        return amount_to_sell_or_buy
+    def cash_wallet_change(percent_change, current_price):
+        if is_asset_enough(percent_change, current_price):
+            # if we have enough assets to buy or sell without our money or bitcoin going below 0
+            if percent_change < 0:
+                # if price goes down we buy the dip, which means our money will go down
+                money_owned_currently = cash_wallet[len(cash_wallet) - 1]
+                amount_to_buy = money_owned_currently * percent_change * -1
+                new_money_owned = money_owned_currently - amount_to_buy
+                cash_wallet.append(new_money_owned)
+                cash_change.append(amount_to_buy)
+                percent_changes.append(percent_change)
+            else:
+                # if price goes up we take profit and sell, which means our money will go up
+                money_owned_currently = cash_wallet[len(cash_wallet) - 1]
+                amount_to_sell = money_owned_currently * percent_change
+                new_money_owned = money_owned_currently + amount_to_sell
+                cash_wallet.append(new_money_owned)
+                cash_change.append(amount_to_sell)
+                percent_changes.append(percent_change)
+        else:
+            # if our bitcoin or cash will dip below 0 with action than we dont do anything and fill our lists
+            # accordingly
+            cash_wallet.append(cash_wallet[len(cash_wallet) - 1])
+            cash_change.append(0)
+            percent_changes.append(percent_change)
+
+    def bitcoin_wallet_change(percent_change, current_price):
+        if is_asset_enough(percent_change, current_price):
+            # if we have enough assets to buy or sell without our money or bitcoin going below 0
+            if percent_change < 0:
+                # if price goes down we buy the dip, which means our bitcoin goes up
+                money_owned_currently = cash_wallet[len(cash_wallet) - 1]
+                bitcoin_currently_owned = bitcoin_wallet[len(cash_wallet) - 1]
+                amount_to_buy_dollars = money_owned_currently * percent_change * -1
+                # network fee deducted from investment amount
+                network_fee = amount_to_buy_dollars * 0.02
+                amount_to_buy_bitcoin = (amount_to_buy_dollars - network_fee) / current_price
+                new_bitcoin_holding = bitcoin_currently_owned + amount_to_buy_bitcoin
+                bitcoin_wallet.append(new_bitcoin_holding)
+                bitcoin_change.append(amount_to_buy_bitcoin)
+                transaction_fee.append(network_fee)
+            else:
+                # if price goes up we take profit and sell, which means our bitcoin will go down
+                money_owned_currently = cash_wallet[len(cash_wallet) - 1]
+                bitcoin_currently_owned = bitcoin_wallet[len(cash_wallet) - 1]
+                amount_to_sell_dollars = money_owned_currently * percent_change
+                # network fee deducted from investment amount
+                network_fee = amount_to_sell_dollars * 0.02
+                amount_to_sell_bitcoin = (amount_to_sell_dollars - network_fee) / current_price
+                new_bitcoin_holding = bitcoin_currently_owned - amount_to_sell_bitcoin
+                bitcoin_wallet.append(new_bitcoin_holding)
+                bitcoin_change.append(amount_to_sell_bitcoin)
+                transaction_fee.append(network_fee)
+        else:
+            # if our bitcoin or cash will dip below 0 with action than we don't do anything and fill our lists
+            # accordingly
+            bitcoin_wallet.append(bitcoin_wallet[len(bitcoin_wallet) - 1])
+            bitcoin_change.append(0)
+            transaction_fee.append(0)
 
     def update_lists(date):
         transaction_date.append(date)
         transaction_bitcoin_price.append(current_price(date))
-
-    def bitcoin_wallet_change(cash_wallet_change, current_price):
-        global bitcoin_wallet
-        current_bitcoin_owned = bitcoin_wallet[len(bitcoin_wallet) - 1]
-        if cash_wallet_change > 0:
-            new_bitcoin_change = cash_wallet_change / current_price * -1
-            new_bitcoin_owned = current_bitcoin_owned - new_bitcoin_change
-            if new_bitcoin_owned < 0:
-                cash_wallet.pop()
-                cash_wallet.append(cash_wallet[len(cash_wallet)-1])
-                bitcoin_wallet.append(bitcoin_wallet[len(bitcoin_wallet)-1])
-                bitcoin_change.append(0)
-            else:
-                bitcoin_wallet.append(new_bitcoin_owned)
-                bitcoin_change.append(new_bitcoin_change)
-        else:
-            new_bitcoin_change = cash_wallet_change / current_price
-            bitcoin_change.append(new_bitcoin_change)
-            new_bitcoin_owned = current_bitcoin_owned + new_bitcoin_change
-            bitcoin_wallet.append(new_bitcoin_owned)
-
 
     def results():
         df = pd.DataFrame({
@@ -92,7 +126,7 @@ def scenario2(filename):
             'percent_changes': percent_changes,
             'transaction_fee': transaction_fee,
             'bitcoin_change': bitcoin_change,
-            'cash_change': cash_change,
+            'cash_change': cash_change
         })
         change_directory = filename.replace('Price_History', "Results")
         excel_file = change_directory.replace(".csv", "Results.xlsx")
@@ -113,8 +147,8 @@ def scenario2(filename):
             date = date.strftime("%Y-%m-%d")
 
             if day % 7 == 0:
-                bitcoin_wallet_change(cash_wallet_change(percent_change(current_price(date), last_week_price(date))),
-                                      current_price(date))
+                bitcoin_wallet_change(percent_change(current_price(date), last_week_price(date)), current_price(date))
+                cash_wallet_change(percent_change(current_price(date), last_week_price(date)), current_price(date))
                 update_lists(date)
                 day += 1
             else:
